13a1a372d4d81b502c6a675ee8b549c2187fb968
Aniroel <lrcbaybay@gmail.com>
2016-03-14 08:51:36 -0700

    add User Book database

--- server/models/book.js
+++ server/models/book.js
@@ -11,8 +11,20 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
+        console.log("BOOK-start");
+        Book.hasMany(models.UserBook, {foreignKey:'bookId'})
+        // Book.belongsToMany(models.User, {
+        //   through: { //for many to many relationships
+        //     model: models.UserBook,
+        //     // unique: true,
+        //   },
+        //   // foreignKey: 'bookId',
+        //   // constraints: false
+        // });
+        console.log("BOOK-end");
       }
     }
   });
+
   return Book;
 };
--- server/models/user.js
+++ server/models/user.js
@@ -8,9 +8,40 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
+        console.log("USER-start");
+        User.hasMany(models.UserBook, {foreignKey: 'userId'});
+        // User.belongsToMany(models.Book, {
+        //   through: { //for many to many relationships
+        //     model: models.UserBook,
+        //     // unique: true,
+        //   },
+        //   // foreignKey: 'userId',
+        //   // constraints: false
+        // });
+        console.log("USER-end");
       }
     }
   });
   return User;
 };
+
+
+// TODO: PUT IN CONTROLLER
+// This finds all books that a user owns in 2 steps
+// 1 - find the UserBooks (join table records)
+// 2 - find the Books for each UserBook
+UserBook.findAll({
+  where: {userId: 1}
+}).then(function(userBooks) {
+  bookIds = userBooks.map(function(ub) { return ub.bookId });
+  Book.findAll({
+    where: {id: bookIds}
+  }).then(function(books) {
+    res.json(books)
+  })
+})
+
+// there might be some convenient functions that sequelize
+// automatically adds for you like this:
+user.userBooks()
 
