2047e90dbdf400ebc19c9f377422f89924df9d3b
Yasunari Flores <carlosyasu91@gmail.com>
2016-03-16 15:13:15 -0700

    Add required dependency

--- client/components/SignIn.jsx
+++ client/components/SignIn.jsx
@@ -4,7 +4,7 @@ var SignIn = () => (
     <form>
       <table>
         <tr><td>User Name or Email</td></tr>
-        <tr><td> <input type="text" name="username" placeholder="Please enter your username"/></td></tr>
+        <tr><td><input type="text" name="username" placeholder="Please enter your username"/></td></tr>
         <tr><td>Password</td></tr>
         <tr><td><input type="password" name="password" placeholder="Please enter your password"/></td></tr>
         <tr><td><input type="submit" value="Log in"/></td></tr>
