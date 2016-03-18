41415b700398682d6c4769419ccef7222f6fc841
Yasunari Flores <carlosyasu91@gmail.com>
2016-03-18 12:30:32 -0700

    Added explore and dashboard links to NavBar

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 class NavBar extends React.Component{
   render(){
     return <nav>
     <ul>
-      <li><a href="#">Home</a></li>
-      <li><a href="#">About</a></li>
+      <li><Link to="/">Home</Link></li>
+      <li><Link to="/explore">Explore</Link></li>
+      <li><Link to="/dashboard">Dashboard</Link></li>
+      <li><a href="/logout">Logout</a></li>
     </ul>
   </nav>
   }
