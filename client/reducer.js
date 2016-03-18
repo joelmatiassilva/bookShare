5deef90d2ab814e8271b001a376dcdb3b5185852
Yasunari Flores <carlosyasu91@gmail.com>
2016-03-17 19:08:32 -0700

    Add setPasswordConfirmataoin and setEmail methods for sign up functionallity

--- client/reducer.js
+++ client/reducer.js
@@ -12,10 +12,29 @@ function setUsername(state, username){
   return state.setIn(['userInfo','username'], username);
 }
 
-function regularLogin(state, info){
+function setEmail(state, username){
+  return state.setIn(['userInfo','email'], username);
+}
+
+function setPasswordConfirmation(state, username){
+  return state.setIn(['userInfo','passwordConfirmation'], username);
+}
+
+function regularLogin(state){
   //TODO make login GET request
   var username = state.getIn(['userInfo', 'username']);
   var password = state.getIn(['userInfo','password']);
+}
+
+function regularSignUp(state, info){
+  var username = state.getIn(['userInfo', 'username']);
+  var email = state.getIn(['userInfo','email']);
+  var password = state.getIn(['userInfo','password']);
+  var passwordConfirmation = state.getIn(['userInfo','passwordConfirmation']);
+  console.log('Username:', username);
+  console.log('email:', email);
+  console.log('password:', password);
+  console.log('passwordConfirmation:', passwordConfirmation);
 }
 
 function facebookLogin(state){
@@ -38,10 +57,16 @@ export default function(state = Map(), action){
       return setState(state, action.state);
     case 'SET_PASSWORD':
       return setPassword(state, action.password);
+    case 'SET_PASSWORD_CONFIRMATION':
+      return setPasswordConfirmation(state, action.password);
     case 'SET_USERNAME':
       return setUsername(state, action.username);
+    case 'SET_EMAIL':
+      return setEmail(state, action.email);
     case 'REGULAR_LOGIN':
       return regularLogin(state);
+    case 'REGULAR_SIGNUP':
+      return regularSignUp(state);
     case 'FACEBOOK_LOGIN':
       return facebookLogin(state);
   }
