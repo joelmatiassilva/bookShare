484eacb224d34ba7bd2ab176f208213d63406480
Yasunari Flores <carlosyasu91@gmail.com>
2016-03-18 11:40:01 -0700

    Added ExploreBooksListEntry component for Explore parent component

---
+++ client/components/ExploreBooksListEntry.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+class ExploreBooksListEntry extends React.Component{
+  render(){
+    return <div>
+      <h1>ExploreBooksListEntry!</h1>
+    </div>;
+  }
+}
+
+export default ExploreBooksListEntry;
