e996845206344d92b80dcff2bd4629e077c17a0a
Yasunari Flores <carlosyasu91@gmail.com>
2016-03-18 12:28:20 -0700

    Added BooksBorroed, BooksLent, MyBooks and MyFriends components to Dashboard component

--- client/components/Dashboard.jsx
+++ client/components/Dashboard.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import NavBar from './NavBar';
+import MyBooks from './MyBooks';
+import MyFriends from './MyFriends';
+import BooksLent from './BooksLent';
+import BooksBorrowed from './BooksBorrowed';
 
 class Dashboard extends React.Component{
   render(){
-    return <h1>
-      Welcome to dashboard!
-    </h1>;
+    return <div>
+        <NavBar/>
+        <h1>Welcome to dashboard!</h1>
+        <MyBooks/>
+        <MyFriends/>
+        <BooksLent/>
+        <BooksBorrowed/>
+      </div>
   }
 }
 
