baa5771354c63137fef5abb224e6bb55fc4369fa
Aniroel <lrcbaybay@gmail.com>
2016-03-19 11:26:45 -0700

    add viewFriendbook functionality

--- server/controllers/bookController.js
+++ server/controllers/bookController.js
@@ -1,5 +1,6 @@
 var models = require('../models');
 var Book = models.Book;
+var UserBook = models.UserBook;
 
 module.exports.addBook= function(req, res){
   Book.create(req.body)
@@ -23,7 +24,7 @@ module.exports.viewMyShelf= function(req, res){
     books = books.map(function(book) {
       return {
         isbn: book.id,
-        author: book.author,
+        author: book.authorm,
         title: book.title,
         description: book.description,
         image: book.image,
@@ -38,10 +39,62 @@ module.exports.viewMyShelf= function(req, res){
 
 };
 
-module.exports.viewFriendsBooks= function(req, res){
+module.exports.viewMyBook= function(req, res){
 // TODO
 };
+
+module.exports.viewFriendBooks= function(req, res){
+// TODO
+};
+
+module.exports.viewFriendsBooks= function(req, res){
+  // TODO
+  req.currentUser.getFriends().then(function(friends) {
+    var friendIds = friends.map(function(friend) { return friend.id; });
+
+    Book.findAll({
+      include: [
+        {model: models.User, where: {id: friendIds}}
+      ]
+    }).then(function(books){
+
+      books = books.map(function(book) {
+        return {
+          // friends is an array of users that have this book.
+          friends: book.Users.map(function(user) {
+            return {
+              id: user.id,
+              name: user.name
+            };
+          }),
+          isbn: book.id,
+          author: book.author,
+          title: book.title,
+          description: book.description,
+          image: book.image,
+          genre: book.genre
+        };
+      });
+      console.log(books);
+      res.status(200).json(books);
+    })
+    .catch(function(err) {
+      console.log(err);
+      res.status(500).json(err);
+    });
+  })
+  .catch(function(err) {
+    console.log(err);
+    res.status(500).json(err);
+  });
+};
+
+module.exports.viewFriendBook= function(req, res){
+// TODO
+};
+
 
 module.exports.deleteBook= function(req, res){
 //TODO
 };
+
