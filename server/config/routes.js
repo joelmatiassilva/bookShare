089cc58a5d02be076ff666b61cf3168b70188cb6
Aniroel <lrcbaybay@gmail.com>
2016-03-19 11:25:01 -0700

    set up routes for viewing friendBooks

--- server/config/routes.js
+++ server/config/routes.js
@@ -8,12 +8,14 @@ module.exports = function (app, express) {
   app.post('/api/signUp', userController.addUser);
   app.post('/api/signIn', userController.signIn);
 
-  app.use(helpers.decode);
 
+  app.use(helpers.decode);
   app.post('/api/logout', userController.logout);
   // TODO: security admin or currentUser access only for delete
   // app.delete('/api/users/:id', userController.deleteUser);
 
+
+  //TODO: set up routes for viewing friendBooks
   /*Facebook Login Routes*/
   app.get('/login/facebook', passport.authenticate('facebook'));
   app.get('/login/facebook/return',
@@ -22,16 +24,25 @@ module.exports = function (app, express) {
     res.redirect('/#/explore');
   });
 
+  app.use(helpers.decode);
+
+  app.post('/api/logout', userController.logout);
+
+  // TODO: security admin or currentUser access only for delete
+  // app.delete('/api/users/:id', userController.deleteUser);
+
   /*Friend Routes*/
   app.post('/api/friendRequests', userController.addFriend); //creates a friendRequest
   app.get('/api/friends', userController.viewAllFriends);
   app.get('/api/friends/:id', userController.viewFriend);
 
   /*Book Routes*/
+  app.get('/api/friends/:id/books', bookController.viewFriendBooks); // all books belonging to one friend
   app.post('/api/books', bookController.addBook);
   app.get('/api/books', bookController.viewMyShelf);
-  app.get('/api/friends/books', bookController.viewFriendsBooks); //??
   app.delete('/api/books/:id', bookController.deleteBook);
+  app.get('/api/friendsBooks', bookController.viewFriendsBooks); // all books belonging to all friends
+  app.get('/api/friendsBooks/:id', bookController.viewFriendBook); // one book belonging to one friend
 
   app.use(helpers.errorLogger);
   app.use(helpers.errorHandler);
