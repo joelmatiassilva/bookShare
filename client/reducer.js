4b06a63058e4c14f62a1ee565cf6800021b3390c
Yasunari Flores <carlosyasu91@gmail.com>
2016-03-21 12:24:16 -0700

    Added add book to my shelf reducer call

--- client/reducer.js
+++ client/reducer.js
@@ -11,7 +11,8 @@ import {
 import {
   setFoundBooks,
   requestBooks,
-  receiveBooks} from './reducers/dashboardReducers';
+  receiveBooks,
+  addBookToMyShelf} from './reducers/dashboardReducers';
 
 function setState(state = Map(), newState){
   return state.merge(newState);
@@ -21,12 +22,12 @@ export default function(state = Map(), action){
   switch(action.type){
     case 'SET_STATE':
       return setState(state, action.state);
+    case 'SET_USERNAME':
+      return setUsername(state, action.username);
     case 'SET_PASSWORD':
       return setPassword(state, action.password);
     case 'SET_PASSWORD_CONFIRMATION':
       return setPasswordConfirmation(state, action.password);
-    case 'SET_USERNAME':
-      return setUsername(state, action.username);
     case 'SET_EMAIL':
       return setEmail(state, action.email);
     case 'SET_TOKEN':
@@ -35,6 +36,8 @@ export default function(state = Map(), action){
       return regularLogin(state);
     case 'REGULAR_SIGNUP':
       return regularSignUp(state);
+    case 'ADD_BOOK_TO_SHELF':
+      return addBookToMyShelf(state, action.book);
     case 'SET_FOUND_BOOKS':
       return setFoundBooks(state, action.foundBooks);
     case 'REQUEST_BOOKS':
