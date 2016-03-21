0e94640f1a5411523b0aeee8139be227be2e8ce8
Yasunari Flores <carlosyasu91@gmail.com>
2016-03-20 17:23:33 -0700

    MyBooks component will now render searched books that come from google books API

--- client/components/MyBooks.jsx
+++ client/components/MyBooks.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 import BookList from './BookList';
 import {connect} from 'react-redux';
 import * as actionCreators from '../action_creators';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
 
-class MyBooks extends React.Component{
+export const MyBooks = class MyBooks extends React.Component{
   constructor(props){
     super(props);
-    console.log(props);
+    this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+  }
+  getBooks(){
+    return this.props.foundBooks || [];
   }
   render(){
     return <div>
       <h2>My Books</h2>
-      <input type="text" placeholder="Search for a book" onChange={(event) =>{this.props.searchBooks(event.target)}}/>
+      <input type="text" placeholder="Search for a book" onChange={(event) =>{this.props.fetchBooks(event.target.value)}}/>
       <h3>Found books from search: </h3>
-      <BookList books={this.props.myBooks}/>
+      {this.getBooks().length > 0 ? <BookList books={this.getBooks()}/> : null}
       <h3>Books that I own: </h3>
       <BookList books={this.props.myBooks}/>
     </div>
@@ -21,14 +25,13 @@ class MyBooks extends React.Component{
 }
 
 function mapStateToProps(state){
+  var books = state ? state.getIn(['dashboard', 'foundBooks']) : [];
   return {
-
+    foundBooks: books
   }
 }
 
 export const MyBooksContainer = connect(
   mapStateToProps,
   actionCreators
-)
-
-export default MyBooks;
+)(MyBooks);
