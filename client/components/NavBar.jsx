f7c222cfdfb66711c6d8401a2ad33a89795c3937
Yasunari Flores <carlosyasu91@gmail.com>
2016-03-21 17:10:40 -0700

    Added logout functionallity to logout

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
-import {Link} from 'react-router';
+import {Link, hashHistory} from 'react-router';
 
 class NavBar extends React.Component{
+  logout(){
+    localStorage.clear();
+    hashHistory.push('/signIn');
+  }
   render(){
     return <nav>
     <ul>
       <li><Link to="/">Home</Link></li>
       <li><Link to="/explore">Explore</Link></li>
       <li><Link to="/dashboard">Dashboard</Link></li>
-      <li><a href="/logout">Logout</a></li>
+      <li><a href="/logout" onClick={this.logout}>Logout</a></li>
     </ul>
   </nav>
   }
