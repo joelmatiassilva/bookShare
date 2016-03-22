2e4e4e5bc4aeab82f9326e02eacd6dc7369b92ef
Yasunari Flores <carlosyasu91@gmail.com>
2016-03-21 17:10:05 -0700

    Added debouncer to input

--- client/components/MyBooks.jsx
+++ client/components/MyBooks.jsx
@@ -3,6 +3,7 @@ import BookList from './BookList';
 import {connect} from 'react-redux';
 import * as actionCreators from '../action_creators';
 import PureRenderMixin from 'react-addons-pure-render-mixin';
+import DebounceInput from 'react-debounce-input';
 
 export const MyBooks = class MyBooks extends React.Component{
   constructor(props){
@@ -12,10 +13,13 @@ export const MyBooks = class MyBooks extends React.Component{
   getBooks(){
     return this.props.foundBooks || [];
   }
+  debouncedFetchBooks(){
+
+  }
   render(){
     return <div>
       <h2>My Books</h2>
-      <input type="text" placeholder="Search for a book" onChange={(event) =>{this.props.fetchBooks(event.target.value)}}/>
+      <DebounceInput debounceTimeout={200} type="text" placeholder="Search for a book" onChange={(event) =>{this.props.fetchBooks(event.target.value)}}/>
       <h3>Found books from search: </h3>
       {this.getBooks().length > 0 ? <BookList books={this.getBooks()}/> : null}
       <h3>Books that I own: </h3>
