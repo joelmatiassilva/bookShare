1e4aa33319e2046ad1da048bf4fbef3500993cee
Aniroel <lrcbaybay@gmail.com>
2016-03-23 14:46:14 -0700

    added comparePassword to User model

--- server/models/user.js
+++ server/models/user.js
@@ -1,71 +1,44 @@
 'use strict';
 var Promise = require('bluebird');
 var bcrypt = require('bcrypt-nodejs');
-// var salt = bcrypt.genSaltSync(10); // FIXME: salts should be per-user and need to be stored in database
 
 module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define('User', {
+
+  var User = sequelize.define('User',
+  { //Obj1
     username: DataTypes.STRING,
     email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {
-  //   hooks: {
-  //     beforeCreate: function(user, options) {
-  //       return user.setHashPassword();
-  //     }
-  //   },
+    password: DataTypes.STRING,
+    salt: DataTypes.STRING
+  },
+
+  { //Obj2: INSTANCE and CLASS Methods
     instanceMethods: {
-    //   setHashPassword: function(){
-    //     return User.hashPassword(this.password).bind(this)
-    //       .then(function(hash){
-    //         this.password = hash;
-    //         return this;
-    //       });
-    //   }
-     },
-    classMethods: {
-      // hashPassword: function(password) {
-      //   var cipher = Promise.promisify(bcrypt.hash);
-      //   return cipher(password, salt, null);
-      // },
-      // signIn: function(email, password){
-      //   return new Promise(function (resolve, reject) {
-      //     User.hashPassword(password)
-      //     .then(function(hash){
-      //       User.findAll({where: {email: email, password: hash}})
-      //       .then(function(users) {
-      //         if(users.length === 0) {
-      //           reject("Not found");
-      //         } else {
-      //           resolve(users[0]);
-      //         }
-      //       })
-      //       .catch(reject);
-      //     })
-      //     .catch(reject);
-      //   });
-        signIn: function(email, password){  //FIXME: salts
-          return new Promise(function (resolve, reject) {
-             User.findAll({where: {email: email, password: password}})
-            .then(function(users) {
-              if(users.length === 0) {
-                reject("Not found");
-              } else {
-                resolve(users[0]);
-              }
-          });
-        });
+      generateSalt: function(){
+        this.salt = bcrypt.genSaltSync(10);
       },
+      hashPassword: function(password) {
+        this.password = bcrypt.hashSync(password, this.salt);
+      },
+
+      comparePassword: function(password){
+        return bcrypt.compareSync(password, this.password);
+      }
+
+     },
+
+    classMethods: {
       associate: function(models){
         //constraints set to false for now to prevent sequelize default behavior
         User.belongsToMany(models.Book, {through: 'UserBook', foreignKey: 'userId', constraints: false});
-        //FIXME: error adding more than one friend per user
         User.belongsToMany(models.User, {through: 'FriendRequest', as: 'Friends', foreignKey: 'userId', constraints: false});
         User.belongsToMany(models.User, {through: 'FriendRequest', as: 'Users', foreignKey: 'friendId', constraints: false});
         User.hasMany(models.FriendRequest, {foreignKey: 'userId', constraints: false});
       }
-    }
-  });
+    } //end classMethods;
+  }
+
+  );
   return User;
 };
 
