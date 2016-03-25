56321a25c6831d50b9ddf4946e856b54ceee0efd
Yasunari Flores <carlosyasu91@gmail.com>
2016-03-24 19:06:34 -0700

    Added friendRequest mapping and made component smart

--- client/components/FriendRequests.jsx
+++ client/components/FriendRequests.jsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import * as actionCreators from '../action_creators';
+import {connect} from 'react-redux';
 
-class FriendRequests extends React.Component{
+export const FriendRequests = class FriendRequests extends React.Component{
+  constructor(props){
+    super(props);
+  }
+  componentWillMount(){
+    this.props.getFriendRequestsDoneToMe();
+  }
   render(){
     return <div>
-      <h1>FriendRequests Component</h1>
+      <h1>FriendRequests done to me:</h1>
+      { this.props.friendRequests ? this.props.friendRequests.map(() => {
+        return <p>This is a friendRequest</p>
+      }) : null}
     </div>;
   }
 }
 
-export default FriendRequests;
+function mapStateToProps(state){
+  return {
+    friendRequests: state.dashboard.get('friendRequests')
+  }
+}
+
+export const FriendRequestsContainer = connect(
+  mapStateToProps,
+  actionCreators
+)(FriendRequests);
