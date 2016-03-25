4d74a228b375c3454d044759f594fa4248caff37
Yasunari Flores <carlosyasu91@gmail.com>
2016-03-24 19:07:52 -0700

    Made component smart

--- client/components/PeopleListEntry.jsx
+++ client/components/PeopleListEntry.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import * as actionCreators from '../action_creators';
+import {connect} from 'react-redux';
 
-class PeopleListEntry extends React.Component{
+export const PeopleListEntry = class PeopleListEntry extends React.Component {
   render(){
     return <ul>
-      Username: {this.props.name} Email: {this.props.email}
+      Username: {this.props.name} Email: {this.props.email} 
+      <button onClick={() => this.props.makeFriendRequest(this.props.email)}>Add friend</button>
     </ul>;
   }
 }
-export default PeopleListEntry;
+
+function mapStateToProps(state){
+  return {}
+}
+
+export const PeopleListEntryContainer = connect(
+  mapStateToProps,
+  actionCreators
+)(PeopleListEntry);
