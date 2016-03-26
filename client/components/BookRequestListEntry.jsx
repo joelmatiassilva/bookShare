010e3c791e44cb1b92b3200235d188fa5efdcce1
Yasunari Flores <carlosyasu91@gmail.com>
2016-03-26 12:04:41 -0700

    Fixed username rendering

--- client/components/BookRequestListEntry.jsx
+++ client/components/BookRequestListEntry.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export const BookRequestListEntry = class BookRequestListEntry extends React.Component{
   render(){
     return <li>
-      <p>MICHAEL wants to borrow:</p>
+      <p>{this.props.username} wants to borrow:</p>
       <p>{this.props.title}</p>
       BookRequestId: {this.props.BookRequestId}
       <button>Accept</button>
