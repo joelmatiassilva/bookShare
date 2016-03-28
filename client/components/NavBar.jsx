ff85a7c0233fd10d1cd8ed99f6499e9f0df2adf6
Aniroel <lrcbaybay@gmail.com>
2016-03-27 17:31:41 -0700

    update style for landing page

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -6,7 +6,7 @@ import * as actionCreators from '../action_creators';
 
 export const NavBar = class NavBar extends React.Component{
   logout(){
-    //Fix logout
+    {/*Fix logout*/}
     localStorage.clear();
     hashHistory.push('/signIn');
     this.props.clearState();
@@ -18,7 +18,7 @@ export const NavBar = class NavBar extends React.Component{
     return <nav>
       {this.checkIfLoggedIn() ?
     <ul>
-      <span>Welcome, {localStorage.displayName}</span> //uppercase 1st char
+      <span>Welcome {localStorage.displayName} </span>
       <li><Link to="/explore">Explore</Link></li>
       <li><Link to="/dashboard">Dashboard</Link></li>
       <li><a onClick={this.logout}>Logout</a></li>
