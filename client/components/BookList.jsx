0a92ded09edf01745e8e6c5eaccf8b7dffc963f8
Jonathan Blaising <jblaising@gmail.com>
2016-03-29 16:31:27 -0700

    getallbooksfromfriends will return an 'accepted' field

--- client/components/BookList.jsx
+++ client/components/BookList.jsx
@@ -1,22 +1,12 @@
 import React from 'react';
-import Slider from 'react-slick';
 import {BookListEntryContainer} from './BookListEntry';
 
 class BookList extends React.Component{
   render(){
-    var settings = {
-      dots: true,
-      infinite: true,
-      speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1
-    };
     return <div className="book-list">
-    <Slider {settings}>
       {this.props.books ? this.props.books.map((book) => {
         return <BookListEntryContainer {...book} key={book.id} />
       }) : null}
-      </Slider>
     </div>;
   }
 }
