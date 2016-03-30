1d4c23fbedbeec9dcba4b24ef601dc73de769b78
Jonathan Blaising <jblaising@gmail.com>
2016-03-29 20:53:31 -0700

    basic carousel

--- client/components/BookList.jsx
+++ client/components/BookList.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import Slider from 'react-slick';
 import {BookListEntryContainer} from './BookListEntry';
 
 class BookList extends React.Component{
+  getSettings() {
+    return {
+      dots: true,
+      infinite: true,
+      slidesToShow: 4,
+      slidesToScroll: 1
+    };
+  }
   render(){
-    return <div className="book-list">
-      {this.props.books ? this.props.books.map((book) => {
+
+    return (
+    <div className="book-list">
+      <Slider {...this.getSettings()}>{this.props.books ? this.props.books.map((book) => {
         return <BookListEntryContainer {...book} key={book.id} />
-      }) : null}
-    </div>;
+      }) : null}</Slider>
+    </div>);
   }
 }
 
