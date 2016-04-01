cdfb70cce3341dd2d77ed087d5704929c7afe622
Yasunari Flores <carlosyasu91@gmail.com>
2016-04-01 11:22:43 -0700

    Modified setup bash

--- setup.sh
+++ setup.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
+echo 'Please type your Google Books API Key'
+read googleKey
 apt-get update
 echo 'setup.hs: Finished updating apt-get'
 sudo apt-get install mysql-server
 echo 'setup.hs: Finished installing mysql'
 npm install
 echo 'setup.hs: Finished npm install'
-bower install
+bower install --allow-root
 echo 'setup.hs: Finished bower install'
+cd client
+rm -f keys.example.js
+echo "export default {GOOGLE_BOOKS_KEY: '$googleKey'};" >> keys.js
+cd ..
