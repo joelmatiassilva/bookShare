0a3573531fb203fd9f5156960b1297b4ccc812b4
joelmatiassilva <joelsilva.1987@gmail.com>
2018-11-04 16:25:51 -0300

    add book

--- server/fakedata.js
+++ server/fakedata.js
@@ -2,17 +2,20 @@ var data = {};
 
 var books = [
   {
-    isbn: '',
-    author: '',
-    title: '',
-    description: '',
-    image: '',
-    genre: ''
+    isbn10: '1234567890',
+    isbn13: '1234567890123',
+    authors: 'J.K. Rowling',
+    title: 'Harry Potter 10',
+    description: 'Best Wizard Ever!',
+    image: 'insertPictureHere',
+    categories: 'Fiction'
   }
 ];
 
 var users = [
 
 ];
+
+data = { books }
 
 module.exports = data
